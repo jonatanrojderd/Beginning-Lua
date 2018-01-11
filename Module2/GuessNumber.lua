5fc8cd7882ed84ee280b0523394c978699d61587
Jonatan Röjder Delnavaz <jonatanrojderd@gmail.com>
2018-01-11 15:38:57 +0100

    * Implemented while-loop to guess the number more than once

--- Module2/GuessNumber.lua
+++ Module2/GuessNumber.lua
@@ -4,14 +4,18 @@ math.randomseed(os.time());
 math.random();
 
 number = math.random(100);
-answer = io.read("*n");
 
-if answer < number then
-	print("Too low");
-elseif answer > number then
-	print("Too high");
-else
-	print("You got it!");
+while answer ~= number do
+
+	answer = io.read("*n");
+
+	if answer < number then
+		print("Too low");
+	elseif answer > number then
+		print("Too high");
+	else
+		print("You got it!");
+	end
 end
 
 print(number);
