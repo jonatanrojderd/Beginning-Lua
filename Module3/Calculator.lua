af0d97f9af7a994603fefe927f4ace4ff5741017
Jonatan Röjder Delnavaz <jonatanrojderd@gmail.com>
2018-01-12 10:06:26 +0100

    * Created a simple calculator

---
+++ Module3/Calculator.lua
@@ -0,0 +1,30 @@
+function adder(x, y)
+	return x + y;
+end
+
+function subtractor(x, y)
+	return x - y;
+end
+
+function calculate(x, y, f)
+	return f(x, y);
+end
+
+print "Enter a number";
+local x = io.read("*n", "*l");
+
+print "Enter another number";
+local y = io.read("*n", "*l");
+
+print "Do you want to add or subtract? (Type + or -)";
+local op = io.read(1, "*l");
+
+if op == "-" then
+	operation = substractor(x, y);
+elseif(op == "+") then
+	operation = adder(x, y);
+else
+	print("Not a valid value!");
+end
+
+calculate(x, y, operation);
