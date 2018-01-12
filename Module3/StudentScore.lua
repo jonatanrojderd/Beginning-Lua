fe5ee726423ea19499ae096fa9cb6e5284c3e4e1
Jonatan Röjder Delnavaz <jonatanrojderd@gmail.com>
2018-01-12 10:24:00 +0100

    * Using a table to store key-value pairs

---
+++ Module3/StudentScore.lua
@@ -0,0 +1,20 @@
+local grades = {};
+while true do
+	print "Enter student name (q to quit)";
+	local name = io.read();
+
+	if name == "q" then
+		break;
+	end
+
+	print "Enter student score";
+	local score = io.read("*n", "*l");
+
+	grades[name] = score;
+end
+
+print "Printing grades!";
+for k, v in pairs(grades) do
+	print "Grade:";
+	print(k, v);
+end
