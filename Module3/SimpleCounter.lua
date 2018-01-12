2450e1d4882ba69228865604d81facf9320b04be
Jonatan Röjder Delnavaz <jonatanrojderd@gmail.com>
2018-01-12 10:15:45 +0100

    * Created a simple counter that uses closures with an iterator

---
+++ Module3/SimpleCounter.lua
@@ -0,0 +1,16 @@
+function simpleCounter(max)
+	local count = 0;
+	return function()
+		count = count + 1;
+		if count > max then
+			return nil;
+		end
+
+		return count;
+	end
+end
+
+for v in simpleCounter(50) do
+	print(v)
+end
+
