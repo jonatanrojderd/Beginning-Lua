25ffb443568c0d6b66d04683990b893676a30d5f
Jonatan Röjder Delnavaz <jonatanrojderd@gmail.com>
2018-01-16 14:44:34 +0100

    * Implemented a stack and queue * Learned how to add and remove elements

---
+++ Module5/TableLibrary.lua
@@ -0,0 +1,28 @@
+---[[
+stack = {};
+table.insert(stack, 32);
+print(unpack(stack));
+
+table.insert(stack, 15);
+print(unpack(stack));
+
+v = table.remove(stack);
+print(v);
+
+table.insert(stack, 13);
+table.insert(stack, 11);
+print(unpack(stack));
+--]]
+
+queue = {};
+table.insert(queue, 1, "Joe");
+table.insert(queue, 1, "Bob");
+table.insert(queue, 1, "Mary");
+
+print(unpack(queue));
+
+table.remove(queue);
+print(unpack(queue));
+
+table.remove(queue);
+print(unpack(queue));
