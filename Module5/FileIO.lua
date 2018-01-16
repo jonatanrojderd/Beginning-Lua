98fc752d6095f18fb8b50e20c876a87ac28d5c9d
Jonatan Röjder Delnavaz <jonatanrojderd@gmail.com>
2018-01-16 14:56:34 +0100

    * Learned how to read and iterate through a file

--- Module5/FileIO.lua
+++ Module5/FileIO.lua
@@ -1,4 +1,14 @@
+--[[
 local file = io.open("test.txt", "w");
 file:write("Testing 1 2 3\n");
 file:write("A B C 1 2 3\n");
 file:close();
+--]]
+
+local file = io.open("test.txt", "r");
+--[[text = file:read("*a");
+print(text);--]]
+
+for line in file:lines() do
+	print(line);
+end
