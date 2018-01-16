09a6cf47c1d10f88cd8a2603c56fd3ccab022f1a
Jonatan Röjder Delnavaz <jonatanrojderd@gmail.com>
2018-01-16 14:05:31 +0100

    * Created People.lua - data * Learned how to load a file and read it's data

--- Module5/LoadSomeCode.lua
+++ Module5/LoadSomeCode.lua
@@ -3,4 +3,10 @@ file = "../Module3/GuessNumber.lua";
 --[[g = loadfile(file);
 g();]]--
 
-dofile(file);
+--dofile(file);
+
+function Person(p)
+	print(p.Name, p.Age, p.Occupation);
+end
+
+dofile("People.lua");
